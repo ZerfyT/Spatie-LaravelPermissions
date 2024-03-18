4be57381668ddf295484c5595fc655a7221f72b1
NipunLiyanage69 <nipun.rightmo@gmail.com>
2024-03-18 07:04:28 +0530

    Added Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call(PermissionTableSeeder::class);
+        $this->call(CreateAdminUserSeeder::class);
     }
 }
